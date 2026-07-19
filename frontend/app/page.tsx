f05b27825e2d9558ef469810b3be7dcc5ae2fb24
mmislam1 <mmislam272@gmail.com>
2026-07-19 18:52:57 +0600

    add meal as home

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,15 @@
 "use client";
 
-import RingChart from "./components/ringChart";
-import Water from "./components/water";
 import StepCounter from "./components/stepCounter";
-import WeightUpdater from "./components/weightUpdater";
+import Meals from "./components/meals";
+import Water from "./components/water";
 
 export default function Home() {
   return (
-    <div>
-    <div className="w-full font-sans flex flex-col">
-      <Water></Water>
+    <div className="mx-auto my-6 flex w-full max-w-xl flex-col gap-4 px-2 sm:px-4">
+      <Meals />
+      <Water />
       <StepCounter />
-      <WeightUpdater />
-      <RingChart></RingChart>
-
-    </div>
-    
     </div>
   );
 }
